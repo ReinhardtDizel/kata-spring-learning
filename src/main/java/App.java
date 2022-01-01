3f4ffdcc2ed9d817de6f399177601a46243ef492
Michael <dize882@gmail.com>
2022-01-01 22:36:39 +0300

    Java pre-project. Практическая задача 2.1.2

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,10 +3,21 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class App {
     public static void main(String[] args) {
+        
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
+
+        HelloWorld bean = (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld beanTwo = (HelloWorld) applicationContext.getBean("helloworld");
+        Cat catBeanOne = (Cat) applicationContext.getBean("cat");
+        Cat catBeanTwo = (Cat) applicationContext.getBean("cat");
+
         System.out.println(bean.getMessage());
+        System.out.println(beanTwo.getMessage());
+        System.out.println(catBeanOne.getMessage());
+        System.out.println(catBeanTwo.getMessage());
+
+        System.out.printf("Тест бина HelloWorld %s\n", bean == beanTwo ? "Пройден" : "Провален");
+        System.out.printf("Тест бина Cat %s\n", catBeanOne == catBeanTwo ? "Провален" : "Пройден");
     }
 }
